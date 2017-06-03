e050fc1ba2832c30580d08a4b9bdffaaf9fa9ff1
kathicks <kmhicks92@gmail.com>
2017-06-03 15:37:02 +0100

    Amended last two exercises so that work if n is 0

--- src/Triangle.java
+++ src/Triangle.java
@@ -5,16 +5,16 @@ public class Triangle {
     }
 
     public String horizontalLine(int num) {
-        String result = "*";
-        for (int i = 1; i < num; i++) {
+        String result = "";
+        for (int i = 0; i < num; i++) {
             result += "*";
         }
         return result;
     }
 
     public String verticalLine(int num) {
-        String result = "*\n";
-        for (int i = 1; i < num; i++) {
+        String result = "";
+        for (int i = 0; i < num; i++) {
             result += "*\n";
         }
         return result;
