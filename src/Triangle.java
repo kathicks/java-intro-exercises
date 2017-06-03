27d7e8bce25be9726063c4f313bfc60be4d349e3
kathicks <kmhicks92@gmail.com>
2017-06-03 15:28:55 +0100

    Added test and code for horizontal line exercise

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,4 +3,12 @@ public class Triangle {
     public String easy() {
         return "*";
     }
+
+    public String horizontalLine(int num) {
+        String result = "*";
+        for (int i = 1; i < num; i++) {
+            result += "*";
+        }
+        return result;
+    }
 }
--- src/TriangleTestDrive.java
+++ src/TriangleTestDrive.java
@@ -8,6 +8,10 @@ public class TriangleTestDrive {
         System.out.print("Exercise 1: ");
         check(triangle.easy(), "*");
         System.out.println(triangle.easy());
+
+        System.out.print("Exercise 2: ");
+        check(triangle.horizontalLine(8), "********");
+        System.out.println(triangle.horizontalLine(8));
     }
 
     private static void check(String method, String result) {
